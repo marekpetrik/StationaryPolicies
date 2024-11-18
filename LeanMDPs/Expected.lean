89f013165e08d8184435a7dc1018090a22ec065b
Marek Petrik <mpetrik@cs.unh.edu>
2024-11-18 16:32:38 -0500

    cleanup

--- LeanMDPs/Expected.lean
+++ LeanMDPs/Expected.lean
@@ -12,23 +12,23 @@ variable {m : MDP σ α}
 Value function type for value functions that are
 
 - history-dependent
-- 
+- for a specific policy
 
 -/
-def ValueHist_π (σ α : Type) := Hist σ α × ℕ → ℝ
+def ValueHist_π := Hist m × ℕ → ℝ
 
 /--
 Defines the value function for a fixed history-dependent policy
 -/
 noncomputable 
-def value_π (π : Policy m) : ValueHist_π σ α := 
+def value_hist_π (π : Policy m) : ValueHist_π (m:=m) := 
       fun ⟨ pre, T ⟩ =>
-        let ha := PHist m pre T
-        ∑ (h ∈ ha), (fun h => (probability m π h) * (reward m h)) h
+        let ha := PHist pre T
+        ∑ (h ∈ ha), (fun h => (probability π h) * (reward h)) h
     -- the charater above is NOT Σ but is ∑ typed as \sum
 
 
-def DP.value_π (π : Policy m)  :=  sorry
+--def DP.value_π (π : Policy m)  :=  sorry
 
 -- theorem value_hist_π_dp (m : MDP σ α) (π : Policy) (T : ℕ) :
               
--- LeanMDPs/Histories.lean
+++ LeanMDPs/Histories.lean
@@ -19,84 +19,12 @@ In this file we define histories and operations that are related to them.
 * A general randomized history-dependent policy
 * The reward and probability of the history, which is used to compute the value function
 * Value function for a history as the expected reward
-
 -/
 
-#check PMF
-#check Finset
-#check Sigma 
-#check Multiset.sum
-#check Set
-
-#eval 1 ∈ [1,2,3] 
-#check Membership.mem [1,2,3] 1
- 
-#check Inhabited
-#check Embedding
 
 variable {σ α : Type}
 variable [Inhabited σ] [Inhabited α]
-variable [DecidableEq σ] [DecidableEq α]
 
-/--
-Represents a history. The state is type σ and action is type α.
-
-The representation of the history is backwards to facilitate the 
-application of a policy
--/
-inductive Hist (σ α : Type)  : Type where
-  | init : σ → Hist σ α
-  | prev : Hist σ α → α → σ → Hist σ α  
-
-/--
-The length of the history corresponds to the zero-based step of the decision
--/
-def Hist.length : Hist σ α → ℕ
-  | init _ => 0
-  | prev h _ _ => HAdd.hAdd (length h) 1
-
-/-- returns the last state of the history -/
-def Hist.laststate  : Hist σ α → σ
-  | init s => s
-  | prev _ _ s => s
-
-/-- appends the state and action to the history --/
-def Hist.append {σ α : Type} (h : Hist σ α) (as : α × σ) : Hist σ α :=
-  Hist.prev h as.fst as.snd
-  
-/--
-Creates new histories from combinations of shorter histories
-and states and actions.
--/
-def append_hist : Hist σ α × α × σ → Hist σ α 
-  | ⟨h, as⟩ => h.append as
-
-def append_hist_linv : Hist σ α → Hist σ α × α × σ
-  | Hist.prev h a s => ⟨ h, a, s ⟩
-  -- the second case is not used
-  | Hist.init _ => ⟨ (Hist.init default), default, default ⟩
-
-/-- Proves that history append has a left inverse. This is used 
-    to show that the append_hist is an embedding, useful when 
-    constructing a set of histories -/
-lemma linv_append_hist {σ α : Type}  [Inhabited σ] [Inhabited α] : 
-    Function.LeftInverse (append_hist_linv (σ := σ) (α := α)) append_hist := fun _ => rfl
-
-/--
-checks if pre is the prefix of h. This is needed when defining value functions
--/
-def isprefix [DecidableEq σ] [DecidableEq α] (pre : Hist σ α) (h : Hist σ α) : Prop :=
-  match pre, h with
-    | Hist.init s₁, Hist.init s₂ => s₁ = s₂
-    | Hist.init _, Hist.prev hp _ _ => isprefix pre hp 
-    | Hist.prev _ _ _, Hist.init _ => False
-    | Hist.prev h₁ a₁ s₁', Hist.prev  h₂ a₂ s₂' => 
-        if h₁.length > h₂.length then
-            False
-        else if h₁.length < h₂.length then
-            isprefix pre h₂
-        else
-            (a₁ = a₂) ∧ (s₁' = s₂') ∧ (isprefix h₁ h₂)
 
 open NNReal -- for ℝ≥0 notation
 
@@ -123,16 +51,79 @@ structure MDP (σ α : Type) : Type where
   -- TODO: all these functions need to be only defined for states and actions
   -- should be using a Subtype {s // s ∈ states} and Finset attach?
 
+variable {m : MDP σ α}
+
+
+/--
+Represents a history. The state is type σ and action is type α.
+
+The representation of the history is backwards to facilitate the 
+application of a policy
+-/
+inductive Hist {σ α : Type} (m : MDP σ α)  : Type where
+  | init : σ → Hist m
+  | prev : Hist m → α → σ → Hist m
+
+/--
+The length of the history corresponds to the zero-based step of the decision
+-/
+def Hist.length : Hist m → ℕ
+  | init _ => 0
+  | prev h _ _ => HAdd.hAdd (length h) 1
+
+/-- returns the last state of the history -/
+def Hist.laststate  : Hist m → σ
+  | init s => s
+  | prev _ _ s => s
+
+/-- appends the state and action to the history --/
+def Hist.append (h : Hist m) (as : α × σ) : Hist m :=
+  Hist.prev h as.fst as.snd
+  
+/--
+Creates new histories from combinations of shorter histories
+and states and actions.
+-/
+def append_hist : Hist m × α × σ → Hist m
+  | ⟨h, as⟩ => h.append as
+
+def append_hist_linv : Hist m → Hist m × α × σ
+  | Hist.prev h a s => ⟨ h, a, s ⟩
+  -- the second case is not used
+  | Hist.init _ => ⟨ (Hist.init default), default, default ⟩
+
+/-- Proves that history append has a left inverse. This is used 
+    to show that the append_hist is an embedding, useful when 
+    constructing a set of histories -/
+lemma linv_append_hist : Function.LeftInverse (append_hist_linv (m := m) ) append_hist := fun _ => rfl
+
+/--
+checks if pre is the prefix of h. This is needed when defining value functions
+-/
+def isprefix (pre : Hist m) (h : Hist m) : Prop :=
+  match pre, h with
+    | Hist.init s₁, Hist.init s₂ => s₁ = s₂
+    | Hist.init _, Hist.prev hp _ _ => isprefix pre hp 
+    | Hist.prev _ _ _, Hist.init _ => False
+    | Hist.prev h₁ a₁ s₁', Hist.prev  h₂ a₂ s₂' => 
+        if h₁.length > h₂.length then
+            False
+        else if h₁.length < h₂.length then
+            isprefix pre h₂
+        else
+            (a₁ = a₂) ∧ (s₁' = s₂') ∧ (isprefix h₁ h₂)
+
+
 /--
 A general randomized history-dependent policy
 -/
 structure Policy (m : MDP σ α) : Type where
-  π : Hist σ α → α → ℝ≥0
+  π : Hist m → α → ℝ≥0
   /-- proof that it sums to 1 for all states -/
-  prob : (h : Hist σ α) → (∑ a ∈ m.AA, π h a) = 1
+  prob : (h : Hist m) → (∑ a ∈ m.AA, π h a) = 1
 
 /-- The set of all histories of length T -/
-def HistAll {σ α : Type} (T : ℕ) := { h : Hist σ α | h.length = T }
+def HistAll (T : ℕ) := { h : Hist m | h.length = T }
 
 
 -- Need to prove the function used in the construction is injective
@@ -148,7 +139,7 @@ and states and actions.
 
 The embedding guarantees it is injective
 -/
-def emb_hist_as : Hist σ α × α × σ ↪ Hist σ α :=
+def emb_hist_as : Hist m × α × σ ↪ Hist m :=
  { toFun := append_hist, inj' := Function.LeftInverse.injective linv_append_hist }
 
 /-- 
@@ -156,41 +147,34 @@ Set of all policies that follow history pre.
 Note that this is just a definition of the set and not a specific instance of the set
 
 The function allhist constructs all histories that satisfy the condition of this set
+
+T is the number of steps beyond the history pre
 -/
-def PHist  [DecidableEq σ] [DecidableEq α] 
-          (m : MDP σ α) (pre : Hist σ α) (T : ℕ) : Finset (Hist σ α)  := 
-             if T < pre.length then
-               Finset.empty
-             else if T = pre.length then
-               {pre}
-             else
-               let AS := Finset.product m.AA  m.SS
-               let HAS := Finset.product (PHist m pre (T-1)) AS
-               Finset.map emb_hist_as HAS 
+def PHist (pre : Hist m) (T : ℕ) : Finset (Hist m) := 
+    if T = 0 then {pre}
+    else
+        let AS := Finset.product m.AA  m.SS
+        let HAS := Finset.product (PHist pre (T-1)) AS
+        Finset.map emb_hist_as HAS 
                
 /--
 Computes the probability of a history
 -/
-noncomputable def probability  [DecidableEq σ] (m : MDP σ α) 
-                              (π : Policy m) : Hist σ α → ℝ≥0 
+noncomputable def probability [DecidableEq σ] (π : Policy m) : Hist m → ℝ≥0 
       | Hist.init s => if m.s₀ = s then 1. else 0.
-      | Hist.prev hp a s' => (m.P hp.laststate a s') * (π.π hp a) * (probability m π hp)  
+      | Hist.prev hp a s' => (m.P hp.laststate a s') * (π.π hp a) * (probability π hp)  
 
 /--
 Computes the reward of a history
 -/
-noncomputable def reward  (m : MDP σ α) :  Hist σ α → ℝ 
+noncomputable def reward :  Hist m → ℝ 
     | Hist.init _ => 0.
-    | Hist.prev hp a s'  =>  (m.r hp.laststate a s') + (reward m hp)  
+    | Hist.prev hp a s'  =>  (m.r hp.laststate a s') + (reward hp)  
 
 
 /-- show that history probabilities are actually a probability distribution -/
-lemma probability_dist (m : MDP σ α) (pre : Hist σ α) (π : Policy m) (T : ℕ) : 
-                       (∑ h ∈ PHist m pre T, (fun h => probability m π) h) = 1 := sorry
-
-
-#check Finset.sum
-
+lemma probability_dist [DecidableEq σ] (pre : Hist m) (π : Policy m) (T : ℕ) : 
+                       (∑ h ∈ PHist pre T, (fun h => probability π) h) = 1 := sorry
 
 /-
 
